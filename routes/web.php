0674c95fa2cc754bcfdb1d8162a52006002696ea
RaazDk1992 <dhakal1992raaz@gmail.com>
2025-12-11 05:55:35 +0545

    setup for blogs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,4 +19,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('blog',BlogController::class);
 require __DIR__.'/auth.php';
